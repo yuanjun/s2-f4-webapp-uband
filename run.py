e2de8b025e89bed60a082ce62e5e1788646283b3
JanceY <1252583@tongji.edu.cn>
2017-08-03 10:44:05 +0800

    B18355 add html

--- run.py
+++ run.py
@@ -4,10 +4,19 @@ from flask import render_template
 
 app = Flask(__name__)
 
+@app.route('/')
+def homepage():
+	return render_template('index.html')
+
 @app.route('/b20840')
 def hello_world():
 	return render_template('b20840.html')
-    
+
+
+@app.route('/B18355')
+def B18355():
+	return render_template('B18355.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
