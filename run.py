5163c8c4e0b061f8327904a9ffa0ec9bfd08c931
fisch321 <335721571@qq.com>
2017-08-04 07:23:42 +0800

    update

--- run.py
+++ run.py
@@ -4,9 +4,15 @@ from flask import render_template
 
 app = Flask(__name__)
 
-@app.route('/b20840')
+@app.route('/')
 def hello_world():
-	return render_template('b20840.html')
+	return render_template('index.html')
+
+@app.route('/<string:student_number>/details')
+def details(student_number):
+	return render_template(str(student_number) + '.html')
+
+
     
 
 if __name__ == '__main__':
